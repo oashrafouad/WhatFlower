5931cf9d5eb809ffe86d06f570e8fa47a0d6463a
Omar Ashraf <amourakora2001@gmail.com>
2024-02-16 17:23:49 +0200

    implement camera button

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let picker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        picker.delegate = self
     }
 
-
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        let actionController = UIAlertController(title: "How do you want to choose your photo?", message: nil, preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { action in
+            self.picker.sourceType = .camera
+            self.present(self.picker, animated: true)
+        }
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { action in
+            self.picker.sourceType = .photoLibrary
+            self.present(self.picker, animated: true)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        actionController.addAction(cameraAction)
+        actionController.addAction(photoLibraryAction)
+        actionController.addAction(cancelAction)
+        
+        present(actionController, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        print("finished")
+        let image = info[.originalImage] as? UIImage
+        imageView.image = image
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("cancelled")
+        picker.dismiss(animated: true)
+    }
 }
 
